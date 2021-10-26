a15385652cb05ce4811e509bfb21530d54223836
lahincapie <alejandra612@hotmail.com>
2021-10-25 22:06:28 -0500

    project

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,18 +4,18 @@
 /**
  * partition - finds the partition for the quicksort
  * @array: array to sort
- * @lowest_index: lowest index of the partition to sort
- * @highest_index: highest index of the partition to sort
+ * @li: lowest index of the partition to sort
+ * @hi: highest index of the partition to sort
  * @size: size of the array
  * Return: index of the partition
  */
-size_t partition(int *array, ssize_t lowest_index, ssize_t highest_index, size_t size)
+size_t partition(int *array, ssize_t li, ssize_t hi, size_t size)
 {
 	ssize_t i, j;
 	int aux, pivot;
-	pivot = array[highest_index];
-	i = lowest_index - 1;
-	for (j = lowest_index; j < highest_index; j++)
+	pivot = array[hi];
+	i = li - 1;
+	for (j = li; j < hi; j++)
 	{
 		if (array[j] < pivot)
 		{
@@ -29,11 +29,11 @@ size_t partition(int *array, ssize_t lowest_index, ssize_t highest_index, size_t
 			}
 		}
 	}
-	if (array[highest_index] < array[i + 1])
+	if (array[hi] < array[i + 1])
 	{
 		aux = array[i + 1];
-		array[i + 1] = array[highest_index];
-		array[highest_index] = aux;
+		array[i + 1] = array[hi];
+		array[hi] = aux;
 		print_array(array, size);
 	}
 	return (i + 1);
@@ -42,18 +42,18 @@ size_t partition(int *array, ssize_t lowest_index, ssize_t highest_index, size_t
 /**
  * quicksort - sorts a partition of an array of integers
  * @array: array to sort
- * @lowest_index: lowest index of the partition to sort
- * @highest_index: highest index of the partition to sort
+ * @li: lowest index of the partition to sort
+ * @hi: highest index of the partition to sort
  * @size: size of the array
  */
-void quicksort(int *array, ssize_t lowest_index, ssize_t highest_index, size_t size)
+void quicksort(int *array, ssize_t li, ssize_t hi, size_t size)
 {
 	ssize_t pivot;
-	if (lowest_index < highest_index)
+	if (li < hi)
 	{
-		pivot = partition(array, lowest_index, highest_index, size);
-		quicksort(array, lowest_index, pivot - 1, size);
-		quicksort(array, pivot + 1, highest_index, size);
+		pivot = partition(array, li, hi, size);
+		quicksort(array, li, pivot - 1, size);
+		quicksort(array, pivot + 1, hi, size);
 	}
 }
 
